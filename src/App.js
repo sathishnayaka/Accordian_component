7e31584625ca7315f83e4ad3f71fffff5a83bf74
SATHEESH NAYAKA K A <sathishadipad1998@gmail.com>
2022-01-10 23:02:17 +0530

    accordian_set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+// import logo from './logo.svg'; 
 import './App.css';
+import data from './data';
+import React ,{useEffect, useState} from 'react';
+
+function Accordian({name,info})
+{
+  const [isactive,setactive]=useState(false);
+  return(
+    <div className='accord'>
+    <h3 onClick={()=>{setactive(!isactive)}}>{isactive?'-':'+'}{name}</h3>
+    {isactive && <p>{info}</p>}
+    </div>
+  );
+}
 
 function App() {
+  const [users,setusers]=useState([])
+  useEffect(()=>{
+    if(data){
+      setusers(data);
+    }
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className='container'>
+      {
+        users.map((user)=>(
+            <Accordian name={user.name} info={user.info}/>
+        ))
+      }
+      </div>
+      
+
+      
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,18 @@
+const users=[{
+    id : 1 ,
+    "name" : " Satheesh Nayaka K A",
+    "info": "Hard working engineering graduate , graduated form nmit banglore"
+},
+{
+    id : 2 ,
+    "name" : " SHEKAR",
+    "info": "Hard working engineering graduate , graduated form nmit banglore"
+},
+{
+    id : 3 ,
+    "name" : " MOHITH",
+    "info": "Hard working engineering graduate , graduated form nmit banglore"
+}
+]
+
+export default users;
